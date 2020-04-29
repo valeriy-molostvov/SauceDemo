bfadb886ff793796c1b4de0224147b4766eddc8d
valeriy-molostvov <hidan00789@gmail.com>
2020-04-29 14:01:13 +0300

    Added CheckoutPage

--- src/test/java/Pages/CheckoutPage.java
+++ src/test/java/Pages/CheckoutPage.java
@@ -8,16 +8,11 @@ public class CheckoutPage extends BasePage {
     private static final By FIRST_NAME = By.id("first-name");
     private static final By LAST_NAME = By.id("last-name");
     private static final By ZIP_CODE = By.id("postal-code");
-    private static final By CANCEL_BUTTON = By.cssSelector(".cart_cancel_link");
     private static final By CONTINUE_BUTTON = By.xpath("//input[@type='submit']");
     private static final By CHECKOUT_OVERVIEW = By.xpath("//*[contains(text(),'Checkout: Overview')]");
     private static final By ITEM_QUANTITY = By.cssSelector(".cart_item");
     private static final By FINISH_BUTTON = By.cssSelector(".btn_action");
     private static final By CONFIRMATION_HEADER = By.className("complete-header");
-    private String ERROR_BUTTON_MESSAGE_WITHOUT_FN = "Error: First Name is required";
-    private String ERROR_BUTTON_MESSAGE_WITHOUT_LN = "Error: Last Name is required";
-    private String ERROR_BUTTON_MESSAGE_WITHOUT_ZIP = "Error: Postal Code is required";
-    private static final By ERROR_BUTTON = By.tagName("h3");
 
     private static final String URL = "https://www.saucedemo.com/checkout-step-one.html";
 
